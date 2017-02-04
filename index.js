5364cf8722b325c94322dbf3093fd4d9cdf96ca7
Régis Behmo <regis@behmo.com>
2017-02-04 21:15:20 +0100

    pouac

--- index.js
+++ index.js
@@ -1,60 +1,85 @@
-user = new Object();
-user.img = new Image();
-user.img.src = 'ship.png';
-user.x = 400;
-user.y = 500;
-user.speed = 10;
-user.bullets = new Array();
-user.bulletImg = new Image();
-user.bulletImg.src = 'ship.png';
+(function(){
+
+function create_image(src) {
+    var img = new Image();
+    img.src = src;
+    return img;
+}
+
 canvas = document.getElementById('SHMUP');
 ctx = canvas.getContext('2d');
+user = {
+    img: create_image('ship.png'),
+    x: 400,
+    y: 500,
+    speed: 10,
+    update: function() {
+		this.x += this.dx;
+		this.y += this.dy;
+        // Stay inside screen
+        user.x = Math.min(Math.max(user.x, user.img.width / 2), canvas.width - user.img.width / 2);
+        user.y = Math.min(Math.max(user.y, user.img.height / 2), canvas.height - user.img.height / 2);
+    }
+};
+bullets = [];
 
+function draw_object(obj) {
+	ctx.drawImage(obj.img, obj.x - obj.img.width / 2, obj.y - obj.img.height / 2);
+}
 
-
-var keys = new Array();
+var keys = [];
 function keydown(event) { keys[event.keyCode] = true; }
 function keyup(event) { keys[event.keyCode] = false; }
 document.onkeydown = keydown;
 document.onkeyup = keyup;
-
-
-
-function game_loop()
-{
-	// input
+function get_player_input() {
 	if (keys[32]) {
-		user.bullets.push({
+		bullets.push({
 			x: user.x,
 			y: user.y,
 			dx: 10,
 			dy: 0,
+            img: create_image('bullet.png'),
+            update: function() {
+                this.x += this.dx;
+                this.y += this.dy;
+            }
 		});
 	}
+    user.dx = 0;
+    user.dy = 0;
 	if (keys[37])
-		user.x -= user.speed;
+		user.dx = -user.speed;
 	if (keys[39])
-		user.x += user.speed;
+		user.dx = user.speed;
 	if (keys[38])
-		user.y -= user.speed;
+		user.dy = -user.speed;
 	if (keys[40])
-		user.y += user.speed;
-	user.x = Math.min(Math.max(user.x, user.img.width / 2), canvas.width - user.img.width / 2);
-	user.y = Math.min(Math.max(user.y, user.img.height / 2), canvas.height - user.img.height / 2);
-	
+		user.dy = user.speed;
+}
+
+function game_loop()
+{
+	// input
+    get_player_input();
+
 	// background
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	
-	// bullets
-	for (var idx in user.bullets) {
-		var bullet = user.bullets[idx];
-		bullet.x += bullet.dx;
-		bullet.y += bullet.dy;
-		ctx.drawImage(user.bulletImg, bullet.x - user.bulletImg.width / 2, bullet.y - user.bulletImg.height / 2);
+    // update state
+    user.update();
+    for(var b = 0; b < bullets.length; b++) {
+        bullets[b].update();
+    }
+
+	// draw aobjects
+	draw_object(user);
+    for(var b = 0; b < bullets.length; b++) {
+		draw_object(bullets[b]);
 	}
 	
-	// user ship
-	ctx.drawImage(user.img, user.x - user.img.width / 2, user.y - user.img.height / 2);
 	requestAnimationFrame(game_loop);
-};
+}
+
 game_loop();
+})();
